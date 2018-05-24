4591854a45185110259bc7b3a68553aabcfe5297
ashspider <39323954+ashspider@users.noreply.github.com>
2018-05-24 12:43:33 +0530

    Update __init__.py

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,10 +1,7 @@
-<html lang = "en">
+<html>
 <head>
 <title> Email validator </title>
 <meta charset = "utf-8">
-<meta name = "viewport" content = "width=device-width , initial-scale=1">
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"> </script>
 
 
 import os, requests, json, sys, traceback
